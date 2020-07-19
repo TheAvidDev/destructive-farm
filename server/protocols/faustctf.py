6720b4c8e3a2d9493b409e782b6b6687ddebeabb
TheAvidDev <AvidDev@protonmail.com>
2020-07-18 23:43:07 -0400

    Add faustctf protocol

---
+++ server/protocols/faustctf.py
@@ -0,0 +1,64 @@
+import socket
+
+from server import app
+from server.models import FlagStatus, SubmitResult
+
+
+# Must be lowercase
+RESPONSES = {
+    FlagStatus.SKIPPED: ['teapot', 'brute force', 'no such flag'],
+    FlagStatus.QUEUED: ['went wrong', 'start yet', 'already over', 'unknown team'],
+    FlagStatus.REJECTED: ['format', 'not recognized', 'flag expired', 'own team', 'nop', 'submitted once'],
+    FlagStatus.ACCEPTED: ['thank you'],
+}
+
+READ_TIMEOUT = 5
+APPEND_TIMEOUT = 0.05
+BUFSIZE = 4096
+
+
+def recvall(sock):
+    sock.settimeout(READ_TIMEOUT)
+    chunks = [sock.recv(BUFSIZE)]
+
+    sock.settimeout(APPEND_TIMEOUT)
+    while True:
+        try:
+            chunk = sock.recv(BUFSIZE)
+            if not chunk:
+                break
+
+            chunks.append(chunk)
+        except socket.timeout:
+            break
+
+    sock.settimeout(READ_TIMEOUT)
+    return b''.join(chunks)
+
+
+def submit_flags(flags, config):
+    sock = socket.create_connection((config['SYSTEM_HOST'], config['SYSTEM_PORT']),
+                                    READ_TIMEOUT)
+
+    greeting = recvall(sock)
+    if b'Flag submission server' not in greeting:
+        raise Exception('Flagserver does not greet us: {}'.format(greeting))
+
+    unknown_responses = set()
+    for item in flags:
+        sock.sendall(item.flag.encode() + b'\n')
+        response = recvall(sock).decode().strip()
+        response_lower = response.lower()
+        for status, substrings in RESPONSES.items():
+            if any(s in response_lower for s in substrings):
+                found_status = status
+                break
+        else:
+            found_status = FlagStatus.QUEUED
+            if response not in unknown_responses:
+                unknown_responses.add(response)
+                app.logger.warning('Unknown checksystem response (flag will be resent): %s', response)
+
+        yield SubmitResult(item.flag, found_status, response)
+
+    sock.close()
